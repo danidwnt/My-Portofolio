8c3379c38f30105a465d818894977b1094488d1c
dani <danidewantara2@gmail.com>
2022-04-10 03:48:44 +0700

    update

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -13,7 +13,7 @@ const Nav = () => {
   const [activeNav, setActiveNav] = useState('#')
   return (
     <nav>
-      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : '' }><AiOutlineHome /></a>
+      <a href="#home" onClick={() => setActiveNav('#home')} className={activeNav === '#' ? 'active' : '' }><AiOutlineHome /></a>
       <a href="#about" onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : '' }><AiOutlineUser /></a>
       <a href="#experients" onClick={() => setActiveNav('#experients')} className={activeNav === '#experients' ? 'active' : '' }><BiBook /></a>
       <a href="#service" onClick={() => setActiveNav('#service')} className={activeNav === '#service' ? 'active' : '' }><RiServiceLine /></a>
